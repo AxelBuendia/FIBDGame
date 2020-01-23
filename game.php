c051e0f86dac02f64040296376c9110766f043e2
RedNaK <rednak@interaction-project.net>
2020-01-23 15:20:39 +0100

    change cookie pour session, update of the game data in php

--- game.php
+++ game.php
@@ -6,7 +6,7 @@
 	$dlevel[$l]['enigma'] = "Level 0. Bienvenue dans l'aventure." ;
 	$dlevel[$l]['type'] = "start" ;
 	$dlevel[$l]['answer'] = "solutionDefault" ;
-	$dlevel[$l]['img'] = "test1.pnj" ;
+	$dlevel[$l]['img'] = "test1.png" ;
 	$l++ ;
 
 	$dlevel[$l]['enigma'] = "Level 1. Exemple d'énigme affiche. Lorem ipsum dolores sit amet.  Lorem ipsum dolores sit amet.  Lorem ipsum dolores sit amet.  Lorem ipsum dolores sit amet.  Lorem ipsum dolores sit amet.  Lorem ipsum dolores sit amet. Lorem ipsum dolores sit amet.  Lorem ipsum dolores sit amet.  Lorem ipsum dolores sit amet. La réponse est affiche1." ;
@@ -15,10 +15,10 @@
 	$dlevel[$l]['img'] = "PNJMain.jpg" ;
 	$l++ ;
 
-	$dlevel[$l]['enigma'] = "Level 2. Enigme finale des badges. L'ordre est 345621" ;
+	$dlevel[$l]['enigma'] = "Level 2. Enigme finale des badges. Ordre: Planning, accessibilité, code, son, image, règles. L'ordre est 345621. Lorem ipsum lorem ipsum. Lorem ipsum lorem ipsum." ;
 	$dlevel[$l]['type'] = "badge_enigme" ;
 	$dlevel[$l]['answer'] = "345621" ;
-	$dlevel[$l]['img'] = "test1.pnj" ;
+	$dlevel[$l]['img'] = "test1.png" ;
 	$l++ ;
 
 	$dlevel[$l]['enigma'] = "Level 3. La fin du jeu, c'est gagné." ;
@@ -27,7 +27,7 @@
 	$dlevel[$l]['img'] = "PNJMain.jpg" ;
 	$l++ ;
 
-	echo "{\"enigma\":\"".$dlevel[$Level]['enigma']."\", \"answer\":\"".$dlevel[$Level]['answer']."\", \"type\":\"".$dlevel[$Level]['type']."\"}" ;
+	echo "{\"enigma\":\"".$dlevel[$Level]['enigma']."\", \"answer\":\"".$dlevel[$Level]['answer']."\", \"img\":\"".$dlevel[$Level]['img']."\", \"type\":\"".$dlevel[$Level]['type']."\"}" ;
 	
 	/*
 	switch ($Level){
